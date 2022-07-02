3dd8c16a17692a01a69be846e5e35e1e52648793
mtinsonk <you@example.com>
2022-07-02 18:51:07 -0400

    Updated

--- main.py
+++ main.py
@@ -1,29 +1,24 @@
 #!/usr/bin/env python3
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
 app = Flask(__name__)
 
-@app.route('/')
-def home_welcome():
-	return '<h1>Welcome!</h1>'
-	return '<h2>Ready to do?</h2.>'
-@app.route('/budget/',  methods =["GET", "POST"])
-def budget():
-    return "<h1>Let's get budgeting!!</h1>"
-def do_calculations(housing, savings, bills, transportation, food, misc):
-	if request.method == "POST":
-       # getting input with name = fname in HTML form
-		checking = request.form.get("Checking: ")
-		housing = request.form.get("Housing: ")
-		savings = request.form.get("Savings: ")
-		bills = request.form.get("Bills: ")
-		transportation = request.form.get("Transportation: ")
-		food = request.form.get("Food: ")
-		misc = request.form.get("Misc: ")
-	return housing + savings + bills + transportation + food + misc
-
+@app.route('/',methods = ['POST','GET'])
+def calculate():
+	budget = ''
+	if request.method=='POST' and 'checking' in request.form and 'housing' in request.form and 'savings' in request.form and 'transportation' in request.form and 'bills' in request.form and 'food' in request.form and 'misc' in request.form:
+		c = float(request.form.get('checking'))
+		h = float(request.form.get('housing'))
+		s = float(request.form.get('savings'))
+		t = float(request.form.get('transportation'))
+		b = float(request.form.get('bills'))
+		f = float(request.form.get('food'))
+		m = float(request.form.get('misc'))
+		budget = round(c+h+s+t+b+f+m)	
+	return render_template('index.html', budget=budget)
+	
 @app.route('/about/')
 def about():
     return '<h3>This is a Flask web application intended to work cohesively with a CI/CD pipeline.</h3>'
