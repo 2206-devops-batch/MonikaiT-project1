23dae457593b51d424e93dffcaf15f14428aa6c0
mtinsonk <mtinsonk@gmail.com>
2022-08-09 15:35:51 -0400

    Updated

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ def about():
 if __name__ == "__main__":
     from waitress import serve
     serve(app, host="0.0.0.0", port=8080)
+###
 
-#app.run(debug=False, host='0.0.0.0', port=5000)
+#!app.run(debug=False, host='0.0.0.0', port=5000)
 
